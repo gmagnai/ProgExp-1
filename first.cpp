aa33c73c33043c264015914dcbf6bdde04ebb29c
Тэгшээ <tegsheeshd@gmail.com>
2016-09-06 08:26:11 +0900

    Euklid

--- first.cpp
+++ first.cpp
@@ -13,6 +13,15 @@ double minux(double x, double y) {
     else return b - a;
 }
 
+double XIEX(double x,double y){
+while(x!=y){
+if(x>y)
+ x=x-y;
+else y=y-x;
+}
+return x;
+}
+
 int main() {
     int option;
     cout<<"\t1. Хоёр тооны ялгавар олох";
