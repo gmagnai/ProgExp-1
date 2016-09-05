4ab6662bb0a69e3c53e8e4c785de81f7b4a76338
Magnai <magnai@metatech.mn>
2016-09-06 08:53:45 +0900

    Дууслаа!

--- first.cpp
+++ first.cpp
@@ -23,41 +23,58 @@ double XIEX(double x,double y){
 }
 
 int main() {
-    int option;
-    cout<<"\t1. Хоёр тооны ялгавар олох";
-    cout<<"\t2. Хоёр тооны нийлбэр олох";
-    cout<<"\t3. Хоёр тооны ХИЕХ олох";
-    cout<<endl;
-    cin>>option;
-    
-    double answer;
-    double a,b;
+	int option;
+	
+    do {
+		cout<<"\t1. Хоёр тооны ялгавар олох\n";
+		cout<<"\t2. Хоёр тооны нийлбэр олох\n";
+		cout<<"\t3. Хоёр тооны ХИЕХ олох\n";
+		cout<<"\t0. Гарах\n";
+		cout<<endl;
+		cout<<"Сонголт: ";
+		cin>>option;
+		
+		double answer;
+		double a,b;
 
-
-    cout<<"A: ";
-    cin>>a;
-    cout<<"B: ";
-    cin>>b;
-    cout<<endl<<endl;
-
-    switch(option) {
-        case 1:
-            answer = minux(a,b);
-            cout<<"Хоёр тооны ялгабар нь: ";        
-            break;
-        case 2:
-            answer = sum(a,b);
-            cout<<"Хоёр тооны нийлбэр нь: ";
-            break;
-        case 3:
-            answer = XIEX(a,b);
-            cout<<"Хоёр тооны хамгийн их ерөнхий хуваагч нь: ";
-            break;
-        default:
-            cout<<"Буруу сонголт!\n";
-    }
-    
-    cout<<endl<<endl<<"Хариу: "<<answer<<endl<<endl;
+		switch(option) {
+		    case 1:
+			    cout<<"A: ";
+			    cin>>a;
+			    cout<<"B: ";
+			    cin>>b;
+			    cout<<endl<<endl;
+		        answer = minux(a,b);
+		        cout<<"Хоёр тооны ялгабар нь: ";        
+		        break;
+		    case 2:
+			    cout<<"A: ";
+			    cin>>a;
+			    cout<<"B: ";
+			    cin>>b;
+			    cout<<endl<<endl;
+		        answer = sum(a,b);
+		        cout<<"Хоёр тооны нийлбэр нь: ";
+		        break;
+		    case 3:
+			    cout<<"A: ";
+			    cin>>a;
+			    cout<<"B: ";
+			    cin>>b;
+			    cout<<endl<<endl;
+		        answer = XIEX(a,b);
+		        cout<<"Хоёр тооны хамгийн их ерөнхий хуваагч нь: ";
+		        break;
+            case 0:
+                break;
+                
+		    default:
+		        cout<<"Буруу сонголт!\n";
+		}
+		
+		if(option != 0) cout<<endl<<endl<<"Хариу: "<<answer<<endl<<endl;
+ 	
+    } while(option != 0);   
     
     return 0;
 }
