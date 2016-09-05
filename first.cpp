bfc3fcebc9c89640246805011349d150b4eccd36
Magnai <magnai@metatech.mn>
2016-09-06 08:12:10 +0900

    test

---
+++ first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int option;
+    cout<<"\t1. Хоёр тооны ялгавар олох";
+    cout<<"\t2. Хоёр тооны нийлбэр олох";
+    cout<<"\t3. Хоёр тооны ХИЕХ олох";
+
+    return 0;
+}
